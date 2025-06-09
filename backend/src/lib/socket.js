28e01dd7df018e10ade8e006557cd2fd398d9a7b
akankshnalam02 <sivanalam717@gmail.com>
2025-06-10 00:20:04 +0530

    updated socketjs

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,55 +1,19 @@
-import { Server } from "socket.io";
-
-import http from "http";
-
-import express from "express";
-import exp from "constants";
-
-
-
-const app = express();
-
-
-const server = http.createServer(app);
-
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://chat-app-ten-mu-77.vercel.app"
+];
 
 const io = new Server(server, {
-    cors:{
-        origin:["http://localhost:5173"]
+  cors: {
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else if (/^https?:\/\/[\w.-]+\.vercel\.app\/?$/.test(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS policy: This origin is not allowed."), false);
+      }
     },
+    credentials: true,
+  }
 });
-
-export function getReceverSocketId(recevierId){
-    return userSocketMap[recevierId];
-}
-
-const userSocketMap ={}; // {userId:socketId}
-
-
-io.on("connection",(socket)=>{
-    console.log("A user is connected",socket.id);
-    const userId  = socket.handshake.query.userId;
-
-    if(userId) {
-    userSocketMap[userId]= socket.id;
-  
-    }
-
-    // io.emit() is used to send the events to all the connected clients
-
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
-
-
-    socket.on("disconnect",()=>{
-        console.log("A user disconnected",socket.id);
-      
-         delete userSocketMap[userId];
-        
-        
-
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    });
-});
-
-
-export {io ,app,server};
