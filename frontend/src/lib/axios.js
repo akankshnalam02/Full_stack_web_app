5d793f3eb5892bc02964e2f535456adfb03a729d
akankshnalam02 <sivanalam717@gmail.com>
2025-06-09 21:51:46 +0530

    Update axios baseURL to use VITE_API_URL env variable

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5003/api" : "/api",
-    withCredentials:true,
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  withCredentials: true,
 });
-
-
-
-``
